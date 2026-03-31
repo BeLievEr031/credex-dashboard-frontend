7790c6cdebe9fc11e68287e9280365ec5b4f0e35
sandip-infolabs <sandip@infolabsglobal.ai>
2026-03-31 08:16:07 +0530

    add production URL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api";
 
 const axiosInstance = axios.create({
-    baseURL: API_BASE_URL,
+    baseURL: "https://credex-dashboard-backend.onrender.com/api",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
